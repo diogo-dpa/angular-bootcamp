e889cb54703e6580c38a86f79f3c696584f32a54
Diogo Almazan <diogodpa@outlook.com>
2025-11-13 15:18:39 -0300

    Initialize ViewEncapsulation Angular project with essential files and components. Added .editorconfig, .gitignore, angular.json, package.json, and TypeScript configuration files. Created main application structure including AppComponent, routing, and basic styles. Implemented ProductCard and UserDetails components with templates and styles, along with unit tests for AppComponent.

---
+++ level-1/fundamentos-do-angular/aulas/view-encapsulation/src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CustomButtonComponent } from '../custom-button/custom-button.component';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [CustomButtonComponent],
+  template: `
+    <p>product-card works!</p>
+    <app-custom-button />
+  `,
+  styles: `
+  p {
+    font-weight: bold;
+  }`,
+  encapsulation: ViewEncapsulation.None,
+})
+export class ProductCardComponent {}
